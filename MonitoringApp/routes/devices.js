af67de4fb01a7b1b6c0205ebb699e2ee7a998a20
Amrish Jhaveri <ajhave5@uic.edu>
2018-10-10 19:27:59 -0500

    few changes.

--- MonitoringApp/routes/devices.js
+++ MonitoringApp/routes/devices.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    var devicesJson = [{"deviceId":"1","deviceType":"Location", "deviceStatus": "Healthy","page":"here.html"},{"deviceId":"2","deviceType":"Temperature, Light", "deviceStatus": "Healthy","page":"sensorsPage.html"}];
+    var devicesJson = [{
+        "deviceId": "1",
+        "deviceType": "Location",
+        "deviceStatus": "Healthy",
+        "page": "here.html"
+    }, {"deviceId": "2", "deviceType": "Temperature, Light", "deviceStatus": "Healthy", "page": "sensorsPage.html"}];
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(devicesJson));
 });
